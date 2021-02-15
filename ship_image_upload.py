b20e1b7f9d45e31e60393ddf2f7fe798bdddc7fe
B2BDA <bishwarup1429@gmail.com>
2021-02-15 18:48:54 +0530

    connected mongo db with frontend

--- ship_image_upload.py
+++ ship_image_upload.py
@@ -1,15 +1,23 @@
 from tensorflow import keras
 import numpy as np
 # from tensorflow.keras.preprocessing.image import load_img
-from PIL import Image, ImageOps
+from PIL import Image
 import streamlit as st
+from pymongo import MongoClient
+client = MongoClient("mongodb+srv://admin:admin@cluster0.42ai5.mongodb.net/test")
+
 model = keras.models.load_model("ship_classifier.h5")
 st.title("Ship Classification")
 uploaded_file = st.file_uploader("Choose an image...", type="jpg")
 class_dict = {'1':'Cargo', '2':'Military', '3':'Carrier', '4':'Cruise', '5':'Tankers'}
- 
- 
-print(uploaded_file)
+try:
+    db = client[client.list_database_names()[0]]
+    collection = db.list_collection_names()[0]
+except Exception as e:
+    pass
+finally:
+    pass
+
 if uploaded_file is not None:
     image = Image.open(uploaded_file)
     # this image goes into the predict function of the model
@@ -29,7 +37,13 @@ if uploaded_file is not None:
 
     
     if st.button("Predict"):
-        predict = np.argmax(model.predict(img), axis = -1)
-        predict = class_dict.get(str(np.argmax(model.predict(img), axis = -1)[0]+1))
-        st.write("The ship is a : %s ship"%str(predict))
-        # make a predicted_class variable and append in the above statement
+        rec = db[collection].find_one({'img_name':str(uploaded_file.name)})
+        if rec is None:
+            predict = np.argmax(model.predict(img), axis = -1)
+            predict = class_dict.get(str(np.argmax(model.predict(img), axis = -1)[0]+1))
+            st.write("The ship is a : %s ship"%str(predict))
+            # make a predicted_class variable and append in the above statement
+            db[collection].insert_one({'class': str(predict),'img_name': str(uploaded_file.name)})
+        else:
+            rec = rec.get('class')
+            st.write("The ship is a : %s ship"%str(rec))
